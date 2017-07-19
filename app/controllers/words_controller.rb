f1ce6e212694c0e5e4360a2b33602db11dc4399d
Julio <juliodevasquez@gmail.com>
2017-07-18 19:09:26 -0500

    anagrams funcional

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,5 +6,11 @@ class WordsController < ApplicationController
     @new_word = params[:new_word]
     @anagrams = anagrams_maker(@new_word)
   end
-
+  def anagrams_maker(word)
+    word = canonical(word)
+    anagrams = Word.where("canonical = ?", word)
+  end
+  def canonical(word)
+    word.downcase.chars.sort.join
+  end
 end
